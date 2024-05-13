95c088d78d6718786b4ac0426aa57534792b6584
apurvdhadankar <apurvdhadankar25@gmail.com>
2024-05-13 16:31:32 -0500

    Adding Find common elements in three sorted arrays program

---
+++ Day4/AD_1.java
@@ -0,0 +1,92 @@
+package arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AD_UnionAndInterSectionOfTwoSortedArrays {
+
+	public static void main(String[] args) {
+		int[] a = {1, 3, 4, 5, 7, 9, 11};
+		int[] b = {2, 4, 6, 7, 8, 10};
+		// op: Union-> 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+		// Intersection: 4, 7
+		printUnionAndIntersection(a, b); // two pointers O(a + b)
+		
+		printUnionAndIntersectionUsingSet(a, b); // Time & space O(a + b)
+
+	}
+	
+	private static void printUnionAndIntersectionUsingSet(int[] a, int[] b) 
+	{
+		
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i : a)
+			set.add(i);
+		
+		for (int i : b)
+			set.add(i);
+		
+		System.out.println("\nUnion -> "+set );
+		
+		Set<Integer> interSections = new HashSet<Integer>();
+		for (int i : a)
+			interSections.add(i);
+		
+		System.out.print("InterSection: ");
+		for (int i : b)
+		{
+			if (interSections.contains(i))
+			{
+				interSections.remove(i);
+				 System.out.print(i + " ");
+			}
+		}
+	}
+
+	private static void printUnionAndIntersection(int[] a, int[] b)
+	{
+		int i=0, j=0;
+		System.out.print("Union: ");
+		while (i < a.length && j < b.length)
+		{
+			if (a[i] < b[j])
+				System.out.print(a[i++] + " ");
+			else if (a[i] > b[j])
+				System.out.print(b[j++] + " ");
+			else
+			{
+				// If both are same print any one of them and increment both
+				System.out.print(a[i++] + " ");
+				j++;
+			}
+		}
+		
+		while (i < a.length)
+		{
+			System.out.print(a[i++] + " ");
+		}
+		
+		while (j < b.length)
+		{
+			System.out.print(b[j++] + " ");
+		}
+		
+		i=0;
+		j=0;
+		System.out.print("\nIntersection: ");
+		while (i < a.length && j < b.length)
+		{
+			if (a[i] < b[j])
+				i++;
+			else if (a[i] > b[j])
+				j++;
+			else
+			{
+				// If both are same print any one of them and increment both
+				System.out.print(a[i++] + " ");
+				j++;
+			}
+		}
+	}
+
+}
---
+++ src/arrays/FindCommonElementsInThreeSortedArrays.java
@@ -0,0 +1,131 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class FindCommonElementsInThreeSortedArrays {
+
+	public static void main(String[] args) {
+		int[] a = {1, 3, 4, 5, 6, 7, 8, 9, 10};
+		int[] b = {2, 3, 5, 6, 8, 10};
+		int[] c = {1, 2, 6, 10, 10};
+		
+		 usingTwoPointersTechnique(a, b , c);  // Time : O (n) + O (n) + O(n) = O(n1+n2+n3). space O(n)
+		 System.out.println(); 
+		
+		 usingThreePointersTechnique(a, b , c);
+		 System.out.println();
+		 
+		usingHashSet(a, b ,c);
+		
+		
+
+	}
+
+	private static void usingThreePointersTechnique(int[] a, int[] b, int[] c) 
+	{
+		int i =0, j=0, k=0;
+		System.out.print("Common elements using three pointers: ");
+		while (i< a.length && j<b.length && k < c.length)
+		{
+			if (a[i] == b[j] && b[j] == c[k])
+			{
+				System.out.print(a[i]+" ");
+				i++;
+				j++;
+				k++;
+			}
+			else if (a[i] < b[j])
+			{
+				i++;
+			}
+			else if (b[j] < c[k])
+			{
+				j++;
+			}
+			else 
+			{
+				k++;
+			}
+		}
+		
+	}
+
+	private static void usingHashSet(int[] a, int[] b, int[] c) {
+		HashSet<Integer> set1 = new HashSet<Integer>();
+		HashSet<Integer> set2 = new HashSet<Integer>();
+		
+		for (int i : a)
+		{
+			set1.add(i);
+		}
+		
+		for (int i : b)
+		{
+			set2.add(i);
+		}
+		
+		System.out.print("\nCommon elements using set: ");
+		for (int i : c)
+		{
+			if (set1.contains(i) && set2.contains(i))
+			{
+				// To maintain uniqueness remove elements from set which are present in both set
+				set1.remove(i);
+				set2.remove(i);
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+	private static void usingTwoPointersTechnique(int[] a, int[] b, int[] c) 
+	{
+		System.out.print("\nCommon elements: ");
+		int i=0, j=0, k=0;
+		
+		int max = Math.max(a.length, b.length);
+		max = Math.max(max, c.length);
+		int[] tempArr = new int[max];
+		
+		while (i<a.length && j<b.length)
+		{
+			if (a[i] < b[j])
+			{
+				i++;
+			}
+			else if (a[i] > b[j])
+			{
+				j++;
+			}
+			else
+			{
+				tempArr[k++] = a[i];
+				i++;
+				j++;
+			}	
+		}
+		
+		i = 0;
+		j = 0;
+		while (i<tempArr.length && j<c.length)
+		{
+			if (tempArr[i] < c[j])
+			{
+				i++;
+			}
+			else if (tempArr[i] > c[j])
+			{
+				j++;
+			}
+			else
+			{
+				System.out.print(c[j] + " ");
+				i++;
+				j++;
+			}
+		}
+		
+		//System.out.println(Arrays.toString(tempArr));
+	}
+
+}
